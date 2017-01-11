4c3b35b493bed00a8424b19500f72a8957028be6
petermania <scott@incrediblemachines.net>
2017-01-11 12:44:12 -0500

    Working v1

--- public/js/camera-control.js
+++ public/js/camera-control.js
@@ -1,44 +1,186 @@
-Cesium.BingMapsApi.defaultKey = 'AvwfPlyN7Gj8bEjnME5NEO8CKq8OmwKBoq9NVOCpb8XluFPedOEd1wLallFuQxYT'
+Cesium.BingMapsApi.defaultKey = key
+var server_location='http://localhost:8080'
+
+var socket = io.connect(server_location);
+
 var viewer = new Cesium.CesiumWidget('cesiumContainer',{
   skyAtmosphere:false,
-  resolutionScale:4.0,
+  resolutionScale:1.0,
   contextOptions: {
     webgl:{preserveDrawingBuffer:true}
   }
 })
 
-var longitude = 0
-var latitude = 0
+var longitude = -73.882593
+var latitude = 40.75226
 var height = 100
+var hRes = 2962
+var vRes = 1920
+var bClick = false
 
 var scene=viewer.scene
 var canvas=viewer.canvas
 
 var currentZoom=0
 
+socket.on('connect', function(data) {
+    console.log('connecting')
+    socket.emit('join', 'Hello World from client');
+})
+
+socket.on('results',function(data){
+  $('#lat').val(data.lat)
+  latitude=data.lat
+  $('#long').val(data.long)
+  longitude=data.long
+  currentZoom=14
+  $('#zoomLabel').text(currentZoom)
+  $('#zoomLevel').val(zoom[currentZoom].zoom_level)
+  $('#zoomValue').val(zoom[currentZoom].zoom_value)
+  viewer.camera.flyTo({
+    destination : Cesium.Cartesian3.fromDegrees(longitude, latitude, zoom[currentZoom].zoom_value),
+    duration:0
+  });
+})
+
 $(document).ready(function(){
   viewer.camera.setView({
     destination : Cesium.Cartesian3.fromDegrees(0, 0, 100000000.0)
   })
   viewer.render()
+  $('#zoomLabel').text(currentZoom)
+  $('#zoomLevel').val(zoom[currentZoom].zoom_level)
+  $('#zoomValue').val(zoom[currentZoom].zoom_value)
 })
 
-$('.getImage').click(function(){
-  var fullQuality = viewer.canvas.toDataURL("image/jpeg", 1.0);
-  console.log(fullQuality)
-  var data = fullQuality.replace(/^data:image\/jpeg;base64,/, "");
-    $('<input>').attr({
-      type: 'hidden',
-      name: 'image',
-      value: data,
-      maxlength:'2000000000000000'
-    }).appendTo('#images');
+$('.getImage').click(function(e){
+  e.preventDefault()
+  $("#cesiumContainer").width(hRes).height(vRes)
+  var count=0
+  viewer.camera.flyTo({
+    destination : Cesium.Cartesian3.fromDegrees(longitude, latitude, zoom[count].zoom_value),
+    duration:0
+  })
+  viewer.render()
+  takeImages(count)
+})
+
+$("#locations > button").on("click",function(e){
+  e.preventDefault()
+  $('.dropdown').removeClass("show")
+  $('#folder').val($(this).text())
+  $('#lat').val($(this).attr('lat'))
+  $('#long').val($(this).attr('long'))
+  longitude = $(this).attr('long')
+  latitude = $(this).attr('lat')
+  currentZoom=14
+  $('#zoomLabel').text(currentZoom)
+  $('#zoomLevel').val(zoom[currentZoom].zoom_level)
+  $('#zoomValue').val(zoom[currentZoom].zoom_value)
+  viewer.camera.flyTo({
+    destination : Cesium.Cartesian3.fromDegrees(longitude, latitude, zoom[currentZoom].zoom_value),
+    duration:0
+  });
 })
 
 $("#zoom-level > button").on("click", function(e){
-  e.preventDefault();
-  console.log("clicked")
+  e.preventDefault()
   $('.dropdown').removeClass("show")
-  console.log($(this).text())
+  currentZoom=$(this).text()
+  $('#zoomLabel').text(currentZoom)
+  $('#zoomLevel').val(zoom[currentZoom].zoom_level)
+  $('#zoomValue').val(zoom[currentZoom].zoom_value)
+  viewer.camera.flyTo({
+    destination : Cesium.Cartesian3.fromDegrees(longitude, latitude, zoom[currentZoom].zoom_value),
+    duration:0
+  });
+  // viewer.camera.lookAt(center, new Cesium.Cartesian3(-40.7, 74.0,0));
+  // viewer.camera.setView({
+  //   destination : Cesium.Cartesian3.fromDegrees(40.7128, 74.0058, zoom[currentZoom].zoom_value)
+  // })
+  viewer.render()
   e.stopPropagation();
 });
+
+$('#lat').focusout(function(){
+  if($('#lat').val()!='') {
+    latitude=$('#lat').val()
+    console.log(latitude)
+  }
+})
+
+$('#long').focusout(function(){
+  if($('#long').val()!=''){
+    longitude=$('#long').val()
+    console.log(latitude)
+  }
+})
+
+$('.setClick').click(function(e){
+  bClick=!bClick
+})
+
+$('#cesiumContainer').click(function(e){
+  console.log("clicked")
+  if(bClick){
+    console.log("setting lat long")
+    var mousePosition = new Cesium.Cartesian2(e.clientX, e.clientY)
+    console.log("mouse "+mousePosition)
+    var ellipsoid = viewer.scene.globe.ellipsoid
+    var cartesian = viewer.camera.pickEllipsoid(mousePosition, ellipsoid)
+    console.log("cartesian "+cartesian)
+    if (cartesian) {
+        var cartographic = ellipsoid.cartesianToCartographic(cartesian);
+        var longitudeString = Cesium.Math.toDegrees(cartographic.longitude).toFixed(8);
+        var latitudeString = Cesium.Math.toDegrees(cartographic.latitude).toFixed(8);
+        console.log("longitude "+longitudeString)
+        $('#lat').val(latitudeString)
+        latitude=latitudeString
+        $('#long').val(longitudeString)
+        longitude=longitudeString
+        console.log("longitude "+longitude)
+        bClick=false
+        $('.setClick').removeClass('active focus')
+    }
+  }
+})
+
+$('.search').click(function(e){
+  e.preventDefault()
+  if($('.search_location').val()!=''){
+    socket.emit('search',$('.search_location').val())
+  }
+})
+
+function takeImages(count) {
+  setTimeout(function(){
+    console.log("recording image "+count)
+    var fullQuality = viewer.canvas.toDataURL("image/jpeg", 1.0);
+    // console.log(fullQuality)
+    var data = fullQuality.replace(/^data:image\/jpeg;base64,/, "");
+      $('<input>').attr({
+        type: 'hidden',
+        name: 'image'+count,
+        value: data,
+        maxlength:'2000000000000000'
+      }).appendTo('#images')
+      count++
+      if(count==20) {
+        console.log("ending record loop")
+        $('#sendImages').click()
+        setTimeout(function(){
+          window.location.reload()
+        },10000)
+      }
+      else{
+        setTimeout(function(){
+          viewer.camera.flyTo({
+            destination : Cesium.Cartesian3.fromDegrees(longitude, latitude, zoom[count].zoom_value),
+            duration:0
+          })
+        viewer.render()
+        takeImages(count)
+      },1000)
+      }
+  },3500)
+}
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-// var Cesium = require('cesium')
 var express = require('express')
 var bodyParser = require('body-parser')
 var fs=require('fs')
+var MongoClient = require('mongodb').MongoClient
+var assert = require('assert')
+var zip = require('zip-folder')
+var geocoder = require('geocoder')
+
+var url = 'mongodb://localhost:27017/Cesium'
+var mapKey = 'AvwfPlyN7Gj8bEjnME5NEO8CKq8OmwKBoq9NVOCpb8XluFPedOEd1wLallFuQxYT'
 
 var app = express()
+var zoom
+var locations
 
 app.use(bodyParser.json({limit:10000000000}))
 app.use(bodyParser.urlencoded({ extended: true, limit:10000000000 }))
@@ -14,28 +22,117 @@ var server = app.listen(8080, function () {
     console.log('Listening on port 8080')
 })
 
+var io = require('socket.io')(server)
+
+io.on('connection', function(client) {
+    console.log('Client connected...');
+    client.on('join', function(data) {
+      console.log(data)
+    });
+    client.on('search',function(string){
+      console.log(string)
+      geocoder.geocode(string, function ( err, data ) {
+        if(data.results.length>0){
+          console.log(data.results[0].geometry.location.lat)
+          console.log(data.results[0].geometry.location.lng)
+          io.emit('results', {lat:data.results[0].geometry.location.lat,long:data.results[0].geometry.location.lng})
+        }
+      });
+    })
+});
 
 app.get('/',function(req,res){
-  res.render('index',{ title: 'Image Exporter'})
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err)
+    console.log("Connected successfully to db server to load zoom levels & locations")
+    var loc=db.collection('locations')
+    loc.find().sort({'folder':1}).toArray(function(e,locs){
+      locations=locs
+      var col=db.collection('zoom')
+      col.find().sort({'zoom_level':1}).toArray(function(err,obj){
+        if(obj.length==0){
+          for(var i=0;i<20;i++){
+            col.insertOne({'zoom_level':i,'zoom_value':0})
+          }
+          col.find().sort({'zoom_level':1}).toArray(function(err,obj){
+            zoom=obj
+            db.close()
+            res.render('index',{ title: 'Image Exporter', zoom:JSON.stringify(zoom), locations:locations, key:mapKey})
+          })
+        }
+        else{
+          zoom=obj
+          db.close()
+          res.render('index',{ title: 'Image Exporter', zoom:JSON.stringify(zoom), locations:locations, key:mapKey})
+        }
+      })
+    })
+  })
 })
 
 app.post('/save-images',function(req,res){
   console.log('saving')
-  // res.render('index',{ title: 'Hey'})
-  var base64Data=req.body.image
-  // console.log(req.query)
-  // for(i in req.query){
-  //   base64Data+=req.query[i]
-  // }
-  console.log(base64Data)
-  // var base64Data = req.query.image_data.replace(/^data:image\/png;base64,/, "");
-  var d=new Date()
-  var dir='public/images/'+d.toString()
+  var d
+  if(req.body['folder']!=''){
+    d=req.body['folder']
+    MongoClient.connect(url, function(err, db) {
+      assert.equal(null, err)
+      console.log("Connected successfully to db server to save location")
+      console.log(req.body.folder)
+      console.log(req.body.lat)
+      console.log(req.body.long)
+      var col=db.collection('locations')
+      col.updateOne({'folder':d},{$set:{'lat':req.body.lat,'long':req.body.long}},{upsert:true},function(err,result){
+        db.close()
+      })
+    })
+  }
+
+  else d=new Date.now()
+
+  var dir=__dirname+'/public/images/'+d.toString()
   if (!fs.existsSync(dir)){
     fs.mkdirSync(dir);
-}
-  fs.writeFile(dir+"/image.jpeg", base64Data, 'base64', function(err){
+  }
+  var count=0
+  saveImage(dir, count,req, res)
+})
+
+function saveImage(dir, i, req, res){
+  var base64Data=req.body['image'+i]
+  // console.log(base64Data)
+  // var base64Data = req.query.image_data.replace(/^data:image\/png;base64,/, "");
+  fs.writeFile(dir+"/image_"+i+".jpeg", base64Data, 'base64', function(err){
     // console.log(err)
-    res.redirect('/')
+    i++
+    if(i==20){
+      console.log("done!")
+      zip(dir,dir+'.zip',function(err){
+        if(err){
+          console.log(err)
+          res.redirect('/')
+        }
+        else{
+          res.download(dir+'.zip')
+        }
+      })
+    }
+    else {
+      console.log("starting to save "+i)
+      saveImage(dir, i, req, res)
+    }
+  })
+}
+
+app.get('/change-zoom',function(req,res){
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err)
+    console.log("Connected successfully to db server to save zoom levels")
+    console.log(req.query.zoom_level)
+    var col=db.collection('zoom')
+    col.updateOne({'zoom_level':parseInt(req.query.zoom_level)},{$set:{'zoom_value':parseInt(req.query.zoom_value)}},function(err,result){
+      db.close
+      res.redirect('/')
+    })
   })
 })
